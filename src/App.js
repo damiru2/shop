c5c9ac2801b8c112904fe3ca799be44737d138ab
damiru2 <chalkarovd11@gmail.com>
2023-12-29 20:56:23 +0500

    add new items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-function App() {
+import Items from "./components/Items";
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+        id: 1,
+        title: 'Стул серый',
+        img: 'chair-gray.jpeg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'chairs',
+        price: '49.99'
+        },
+        {
+        id: 2,
+        title: 'Стол',
+        img: 'table.jpeg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'tables',
+        price: '149.00' 
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'sofa', 
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'light.jpeg',
+          desc: 'Ldrem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'light', 
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'sofa', 
+          price: '25.00'
+        }
+      ]
+    }
+  }
+  render() {
   return (
     <div className="wrapper">
     <Header />
+    <Items items={this.state.items} />
     <Footer />
     </div>
   )
+}
 }
 
 export default App;
