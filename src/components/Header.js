95858330fe2e66800c9d139d75149c7159d371bd
damiru2 <chalkarovd11@gmail.com>
2023-12-31 15:18:22 +0500

    add shoping functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa"
+import Order from './Order'
 
-export default function Header() {
+const showOrders = (props) => {
+  return (<div>
+    {props.orders.map(el => (
+      <Order key={el.id} item={el} />
+    ))}
+  </div>)
+}
+
+const showNothing = () => {
+  return (<div className='empty'>
+        <h2>Товаров нет</h2>
+  </div>)
+}
+
+export default function Header(props) {
+   let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header> 
         <div>
@@ -10,6 +28,14 @@ export default function Header() {
                 <li>Контакты</li>
                 <li>Кабинет</li>
             </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen) } className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+            {cartOpen && (
+                <div className='shop-cart'>
+                     {props.orders.length > 0 ?
+                         showOrders(props) : showNothing()}
+                  </div>  
+            )}
         </div>
         <div className='presentation'></div>
     </header>
